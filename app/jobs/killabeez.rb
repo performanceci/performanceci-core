4abd54ecfa7c803a08a4d2e0679b22724e996d98
Jason Thigpen <darwin@senet.us>
2014-02-22 22:01:59 -0800

    Send attacker array of endpoints and be smarter about attacking

--- app/jobs/killabeez.rb
+++ app/jobs/killabeez.rb
@@ -5,25 +5,32 @@ class KillaBeez < Worker
     include Resque::Plugins::Status
     @queue = "beezAttackQueue"
     def perform
-        at(0, 1, "Sleepy Beez")
-        url = options['url']
-        start = Time.now
-        response = Net::HTTP.get_response(URI(url))
+      at(0, 1, "Sleepy Beez")
+      endpoints = options['endpoints']
 
-        case response
-        when Net::HTTPSuccess then
-          response
-        when Net::HTTPRedirection then
-          location = response['location']
-          warn "redirected to #{location}"
-          fetch(location, limit - 1)
-        else
-          response.value
-        end
-        finish = Time.now
-        diff = finish - start
-        completed(:latency => diff)
-        #Worker.system_quietly("#{cmd}")
-        puts "done"
+      respone_time = endpoints.map { |u| attack(u) }
+      completed(:latency => respone_time)
+      puts "done"
+    end
+    def attack(url)
+
+      start = Time.now
+      10.times.each do |time|
+        response = Net::HTTP.get_response(URI(url))
+      end
+      finish = Time.now
+
+
+      case response
+      when Net::HTTPSuccess then
+        response
+      when Net::HTTPRedirection then
+        location = response['location']
+        warn "redirected to #{location}"
+        fetch(location, limit - 1)
+      else
+        response.value
+      end
+      diff = (finish - start) / 10
     end
 end
