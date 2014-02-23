c23bd5485615807a606395dcf54c2f29ba32d569
Gilberto Morejon <gmorejon@evntlive.com>
2014-02-22 19:32:03 -0800

    shellout for docker run command so we can specify 0.0.0.0

--- app/jobs/docker.rb
+++ app/jobs/docker.rb
@@ -16,8 +16,9 @@ class DockerWorker < Worker
       Git.clone(url, path)
       at(1, 3, "Building container")
       image = Docker::Image.build_from_dir(path)
-      container = image.run()
-      job_id = KillaBeez.create(:url => 'http://23.253.97.212:4567/test', :cid => container.id)
+      #container = image.run()
+      Worker.system_quietly("docker -d -p 0.0.0.0:4567:4567 #{image.id}")
+      job_id = KillaBeez.create(:url => 'http://23.253.97.212:4567/test')
       while status = Resque::Plugins::Status::Hash.get(job_id) and !status.completed? && !status.failed?
         sleep 1
         puts status.inspect
