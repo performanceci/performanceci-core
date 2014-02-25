f44b5343067bedbf2fcb887be17b0fc898d873ef
Alex Kira <akira@apple.com>
2014-02-24 16:55:18 -0800

    Thumbs for respon time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def icon_for_status(status)
+    if @build
+      case @build.build_status
+      when 'success'
+        '<i class="fa fa-check fa-fw" style="color:green"></i>'
+      when 'warn'
+        '<i class="fa fa-warning fa-fw" style="color:orange"></i>'
+      when 'error'
+        '<i class="fa fa-warning fa-fw" style="color:red"></i>'
+      when 'failed'
+        '<i class="fa fa-warning fa-fw" style="color:red"></i>'
+      else
+        ''
+      end
+    else
+      ""
+    end
+  end
 end
