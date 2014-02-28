99e074a49a1c2e70fe9ed78eb767f9d93af4e7ea
Jason Thigpen <darwin@senet.us>
2014-02-27 19:50:01 -0800

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,3 +5,7 @@ set -e
 apt-get update
 apt-get upgrade -y
 apt-get install -y git-core build-essential ruby2.0 ruby2.0-dev libsqlite3-dev bundler redis-server
+# Docker host
+# http://askubuntu.com/questions/350657/lxc-start-failed-to-spawn
+# apt-get install -y cgroup-lite
+# sudo cgroups-mount
