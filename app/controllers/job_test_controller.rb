d3136d6602c9167b6648dcf6315b83d555133ee6
Alex Kira <alex.kira@gmail.com>
2014-03-03 21:52:18 -0800

    Cleanup spacing.  Move worker out of directory since it’s not a module

--- app/controllers/job_test_controller.rb
+++ app/controllers/job_test_controller.rb
@@ -1,18 +1,16 @@
-require_relative '../jobs/killabeez.rb'
-require_relative '../jobs/docker.rb'
-
 
 class JobTestController < ApplicationController
-    def swarm
-        KillaBeez.create(:cmd => 'sleep 5')
+  def swarm
+    KillaBeez.create(:cmd => 'sleep 5')
 
-        render text: "KillaBeez on the loose!"
-    end
-    def docker
-      DockerWorker.create(
-        :url => 'https://github.com/performanceci/simple.git',
-        :repo => 'simple'
-      )
-      render text: "Docker is better"
-    end
+    render text: "KillaBeez on the loose!"
+  end
+
+  def docker
+    DockerWorker.create(
+      :url => 'https://github.com/performanceci/simple.git',
+      :repo => 'simple'
+    )
+    render text: "Docker is better"
+  end
 end
