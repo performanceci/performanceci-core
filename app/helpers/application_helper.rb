87552e3bc0f87286c41a5492d01e864887dbb457
Alex Kira <alex.kira@gmail.com>
2014-03-14 09:48:33 -0700

    Build status - fix to display for specific build

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,17 @@
 module ApplicationHelper
 
   def icon_for_status(status)
-    if @build
-      case @build.build_status
-      when 'success'
-        '<i class="fa fa-check fa-fw" style="color:green"></i>'
-      when 'warn'
-        '<i class="fa fa-warning fa-fw" style="color:orange"></i>'
-      when 'error'
-        '<i class="fa fa-warning fa-fw" style="color:red"></i>'
-      when 'failed'
-        '<i class="fa fa-warning fa-fw" style="color:red"></i>'
-      else
-        ''
-      end
+    case status
+    when 'success'
+      '<i class="fa fa-check fa-fw" style="color:green"></i>'
+    when 'warn'
+      '<i class="fa fa-warning fa-fw" style="color:orange"></i>'
+    when 'error'
+      '<i class="fa fa-warning fa-fw" style="color:red"></i>'
+    when 'failed'
+      '<i class="fa fa-warning fa-fw" style="color:red"></i>'
     else
-      ""
+      ''
     end
   end
 end
