34c66ce9fd1c0296a46b42e60bbf3e336b8056a1
Alex Kira <alex.kira@gmail.com>
2014-04-06 23:12:18 -0700

    Devise login - use a proper login screen instead of javascript redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout_by_resource
 
   def after_sign_in_path_for(resource)
     results_overview_index_path
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      "login.html.erb"
+    else
+      "application"
+    end
   end
 end
