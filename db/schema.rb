26d470b5c916ce445a32518d8e066e73792c65c7
Alex Kira <alex.kira@gmail.com>
2014-04-06 18:01:47 -0700

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140224230212) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "build_endpoints", force: true do |t|
     t.integer  "endpoint_id"
     t.integer  "build_id"
@@ -93,7 +96,7 @@ ActiveRecord::Schema.define(version: 20140224230212) do
     t.string   "gravatar_id"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
