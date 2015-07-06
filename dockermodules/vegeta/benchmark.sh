06ac6be02c7c9da4051d77beaa476f484902d0db
Jason Thigpen <crd@luciddg.com>
2015-07-05 18:51:33 -0700

    Update vageta Dockerfile and benchmarking script
    Use official library/golang:1.4.2 base image
    Update GOPATH locations accordingly

--- dockermodules/vegeta/benchmark.sh
+++ dockermodules/vegeta/benchmark.sh
@@ -6,7 +6,7 @@ if [ "$1" == 'container' ]; then
 else
   ENDPOINT="$2"
 fi
-#echo "GET $ENDPOINT | /opt/goroot/bin/vegeta attack -duration=$4 -rate=$3 | tee results.bin | /opt/goroot/bin/vegeta report -reporter=json"
+echo "GET $ENDPOINT | /opt/goroot/bin/vegeta attack -duration=$4 -rate=$3 | tee results.bin | /opt/goroot/bin/vegeta report -reporter=json"
 
-RESULT=$(echo "GET $ENDPOINT" | /opt/goroot/bin/vegeta attack -duration=$4 -rate=$3 | /opt/goroot/bin/vegeta report -reporter=json)
+RESULT=$(echo "GET $ENDPOINT" | /go/bin/vegeta attack -duration=$4 -rate=$3 | /go/bin/vegeta report -reporter=json)
 echo $RESULT
